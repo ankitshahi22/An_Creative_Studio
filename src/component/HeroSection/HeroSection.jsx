a171ab392130d108d26c22b79eb0266645814a79
Ankit Shahi <ankitshahi593@gmail.com>
2026-05-27 11:34:57 +0545

    chnange on hero section border

--- src/component/HeroSection/HeroSection.jsx
+++ src/component/HeroSection/HeroSection.jsx
@@ -9,45 +9,47 @@ const HeroSection = () => {
   return (
     <div className={`${borderClr}`}>
       <motion.div
-        className={`flex justify-center w-[80%] p-6 mx-auto items-center h-svh gap-15 -mt-20`}
+        className={`flex justify-center w-[80%] p-6 mx-auto items-center h-svh gap-15`}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1.5, ease: "easeInOut" }}
       >
-        <div className="flex flex-col gap-1 w-fit">
-          <h2 className="text-3xl font-extralight tracking-tight">
-            Nirjal Timalsina <span className="text-xs">📍 Nepal</span>
-          </h2>
-          <div className="text-7xl font-semibold tracking-tight">
-            <motion.p
-              initial={{ opacity: 0, x: 100 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.8 }}
-            >
-              PROFESSIONAL
-            </motion.p>
-            <motion.p
-              initial={{ opacity: 0, y: 30 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.8, delay: 0.2 }}
-              className="-mt-3"
-            >
-              VIDEO EDITOR
-            </motion.p>
+        <div className="flex gap-10 -mt-20">
+          <div className="flex flex-col gap-1 w-fit">
+            <h2 className="text-3xl font-extralight tracking-tight">
+              Nirjal Timalsina <span className="text-xs">📍 Nepal</span>
+            </h2>
+            <div className="text-7xl font-semibold tracking-tight">
+              <motion.p
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.8 }}
+              >
+                PROFESSIONAL
+              </motion.p>
+              <motion.p
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8, delay: 0.2 }}
+                className="-mt-3"
+              >
+                VIDEO EDITOR
+              </motion.p>
+            </div>
+            <p className="text-lg font-extralight">
+              Making Your Video Look More Cool.
+            </p>
+            <Button />
           </div>
-          <p className="text-lg font-extralight">
-            Making Your Video Look More Cool.
-          </p>
-          <Button />
-        </div>
-        <div>
-          <Image
-            src="/HeroSectionImg.png"
-            alt="hero"
-            loading="eager"
-            width={300}
-            height={300}
-          />
+          <div>
+            <Image
+              src="/HeroSectionImg.png"
+              alt="hero"
+              loading="eager"
+              width={300}
+              height={300}
+            />
+          </div>
         </div>
       </motion.div>
     </div>
