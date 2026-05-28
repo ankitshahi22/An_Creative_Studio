e503975d51dd3e4db9f13ce8149a6956a0c0afda
Ankit Shahi <ankitshahi593@gmail.com>
2026-05-28 13:37:22 +0545

    test push

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -1,14 +1,42 @@
+"use client";
+
 import { borderClr } from "../../store/data";
 import Input from "../../utils/Input";
+import { useState } from "react";
+import { useFormStatus } from "react-dom";
 
 const Contact = () => {
+  const [result, setResult] = useState("");
+  const { pending } = useFormStatus();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+    formData.append("access_key", "2f05bd48-6ad6-4b7e-846f-ba6c1831255c");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      setResult("Error");
+    }
+  };
+
   return (
     <div className={`${borderClr}`}>
       <div className="p-8 w-[80%] mx-auto">
         <h2 className="text-4xl font-bold text-[#007bff] text-center">
           CONTACT
         </h2>
-        <div className="flex flex-col gap-4 py-8">
+        <form className="flex flex-col gap-4 py-8" onSubmit={onSubmit}>
           <div className="flex flex-col gap-6">
             <Input
               label="Full Name"
@@ -29,12 +57,16 @@ const Contact = () => {
             />
           </div>
           <div className="flex justify-end gap-6 text-sm">
-            <button>Reset</button>
-            <button className="px-2 py-1.5 bg-blue-500 text-white rounded">
-              Submit
+            <button type="reset">Reset</button>
+            <button
+              type="submit"
+              className="px-2 py-1.5 bg-blue-500 text-white rounded"
+            >
+              {pending ? "Submitting..." : "Submit"}
             </button>
           </div>
-        </div>
+          <span>{result}</span>
+        </form>
       </div>
     </div>
   );
--- src/utils/Input.jsx
+++ src/utils/Input.jsx
@@ -1,11 +1,10 @@
-const Input = ({ label, name, placeholder, type = "text" }) => {
+const Input = ({ label, name, type = "text" }) => {
   return (
     <div className="flex flex-col">
       <label className="text-gray-700">{label}*</label>
       <input
         type={type}
         name={name}
-        placeholder={placeholder}
         className="border-0 border-b border-gray-500 p-1 focus:outline-none focus:ring-2 focus:ring-transparent placeholder:text-sm text-sm"
       />
     </div>
