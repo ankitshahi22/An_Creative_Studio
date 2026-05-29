c3c4ad89416daa701e664d1fbc565012b7ecf203
Ankit Shahi <ankitshahi593@gmail.com>
2026-05-30 04:51:55 +0545

    new button on project section for more project view

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -32,44 +32,49 @@ const Contact = () => {
 
   return (
     <div className={`${borderClr}`}>
-      <div className="p-8 w-[80%] mx-auto">
+      <div className="flex flex-col items-center gap-8 p-8 w-[80%] mx-auto">
         <h2 className="text-4xl font-bold text-[#007bff] text-center">
-          CONTACT
+          Get in Touch
         </h2>
-        <form className="flex flex-col gap-4 py-8" onSubmit={onSubmit}>
-          <div className="flex flex-col gap-6">
-            <Input
-              label="Full Name"
-              name="fullName"
-              placeholder="Enter your name"
-            />
-            <Input
-              label="Email"
-              name="email"
-              type="email"
-              placeholder="Example@gmail.com"
-            />
-            <Input
-              label="Message"
-              name="message"
-              type="textarea"
-              placeholder="Enter your message"
-            />
-          </div>
-          <div className="flex justify-end gap-6 text-sm">
-            <button type="reset">Reset</button>
-            <button
-              type="submit"
-              className="px-2 py-1.5 bg-blue-500 text-white rounded"
-            >
-              {pending ? "Submitting..." : "Submit"}
-            </button>
-          </div>
-          <span>{result}</span>
-        </form>
+        <button className="px-4 py-1.5 text-sm bg-blue-500 text-white rounded w-fit mx-auto">
+          Send Message
+        </button>
       </div>
     </div>
   );
 };
 
 export default Contact;
+
+{
+  /* <div className="p-8 w-[80%] mx-auto">
+  <h2 className="text-4xl font-bold text-[#007bff] text-center">CONTACT</h2>
+  <form className="flex flex-col gap-4 py-8" onSubmit={onSubmit}>
+    <div className="flex flex-col gap-6">
+      <Input label="Full Name" name="fullName" placeholder="Enter your name" />
+      <Input
+        label="Email"
+        name="email"
+        type="email"
+        placeholder="Example@gmail.com"
+      />
+      <Input
+        label="Message"
+        name="message"
+        type="textarea"
+        placeholder="Enter your message"
+      />
+    </div>
+    <div className="flex justify-end gap-6 text-sm">
+      <button type="reset">Reset</button>
+      <button
+        type="submit"
+        className="px-2 py-1.5 bg-blue-500 text-white rounded"
+      >
+        {pending ? "Submitting..." : "Submit"}
+      </button>
+    </div>
+    <span>{result}</span>
+  </form>
+</div>; */
+}
--- src/component/work/work.jsx
+++ src/component/work/work.jsx
@@ -1,8 +1,10 @@
 "use client";
 
+import Link from "next/link";
 import { borderClr } from "../../store/data";
 import WorkSection from "./WorkSection";
 import { motion } from "framer-motion";
+import { ArrowBendDownRightIcon } from "@phosphor-icons/react";
 
 export default function Work() {
   return (
@@ -16,6 +18,18 @@ export default function Work() {
         PROJECTS
       </h2>
       <WorkSection />
+      <Link href="/" className="text-center">
+        <motion.p
+          whileHover={{
+            scale: 1.1,
+            transition: { duration: 0.2 },
+          }}
+          className="flex items-center gap-2 w-fit mx-auto border border-gray-500 px-2 py-1 rounded-md mb-10 text-sm hover:bg-black/30 hover:text-white"
+        >
+          More Works
+          <ArrowBendDownRightIcon size={16} />
+        </motion.p>
+      </Link>
     </motion.div>
   );
 }
